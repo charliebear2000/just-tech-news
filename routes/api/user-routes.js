390aeba845cec36092ca5b00d7dcdacc0b7956a3
Leah <amelia_rawls@ccpsnet.net>
2022-05-17 04:52:17 -0400

    created password encryption

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -37,7 +37,7 @@ router.get('/:id', (req, res) => {
 
 // POST /api/users
 router.post('/', (req, res) => {
-   // expects {username: 'Lernatino', email: 'lernatino@gmail.com', password: 'password1234'}
+   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
    User.create({
       username: req.body.username,
       email: req.body.email,
@@ -47,6 +47,32 @@ router.post('/', (req, res) => {
    .catch(err => {
       console.log(err);
       res.status(500).json(err);
+   });
+});
+
+// POST api/users/login
+router.post('/login', (req, res) => {
+
+// expects {email: 'lernantino@gmail.com', password: 'password1234'}
+User.findOne({
+   where: {
+      email: req.body.email
+   }
+})
+.then(dbUserData => {
+   if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address.' });
+      return;
+   }
+   // res.json({ user: dbUserData });
+
+   // verify user
+const validPassword = dbUserData.checkPassword(req.body.password);
+   if(!validPassword) {
+      res.status(400).json({ message: 'Incorrect password' });
+      return;
+   }
+   res.json({ user: dbUserData, message: 'You are now logged in.' })
    });
 });
 
