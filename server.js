a074dfc7b44d0764f567701b5721e0e7a26b5e61
Leah <amelia_rawls@ccpsnet.net>
2022-05-27 04:54:27 -0400

    created dashboard view

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
-const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -21,10 +19,15 @@ const sess = {
   })
 };
 
+app.use(session(sess));
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(session(sess));
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
